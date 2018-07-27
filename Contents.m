62631fa18bb140737f699780bdc4a4b14ca74f67
Giuliano Casale <g.casale@imperial.ac.uk>
2018-07-27 22:31:38 +0200

    Update Contents.m

--- Contents.m
+++ Contents.m
@@ -3,7 +3,7 @@
 % Version 2.0.0 beta 1 (July 2018)
 % Website https://github.com/line-solver/line
 %
-% Copyright (C) 2011-2018, Imperial College London, UK,
+% Copyright (C) 2013-2018, Imperial College London, UK,
 %
 % LINE is released under the BSD-3 license.
 %
