69228be68c3780fe23779e7ae1bc85fb17822713
Giuliano Casale <gcasale@ic.ac.uk>
2019-03-18 23:53:15 +0000

    Removed benchmarks

--- bench/allBench.m
+++
@@ -1,8 +0,0 @@
-w=warning;
-warning off
-verbose = 1;
-
-runtests('allBenchCQN_PS','Verbosity', verbose); % evaluate multiclass product-form models
-runtests('allBenchCQN_FCFS','Verbosity', verbose); % evaluate multiclass FCFS approximations
-
-warning(w);
